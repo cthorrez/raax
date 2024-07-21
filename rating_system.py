ffd6eb328a8f1e7706685ab304c1b5eb7c43c335
cthorrez <claytonthorrez@gmail.com>
2024-07-20 21:42:04 -0700

    start setting up the scan

--- rating_system.py
+++ rating_system.py
@@ -4,7 +4,17 @@ import jax.numpy as jnp
 class RatingSystem:
     def __init__(self, competitors, update_fn):
         self.competitors = competitors
+        self.num_competitors = len(competitors)
+        self.ratings = jnp.zeros(shape=self.num_competitors, dtype=jnp.float32)
         self.update_fn = update_fn
 
     def fit(self, time_steps, schedule, outcomes):
-        pass
+        final_state = jax.lax.scan(
+            f = self.update_fn,
+            init = self.ratings,
+            xs = {
+                't' : None
+                'schedule' : schedule,
+                'outcomes' : outcomes,
+            }
+        )
