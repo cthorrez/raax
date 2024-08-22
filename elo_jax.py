417fd1fae24674426d4d2acb25ca2d7b158fdd18
cthorrez <claytonthorrez@gmail.com>
2024-08-21 23:27:55 -0400

    elo online in jax

---
+++ elo_jax.py
@@ -0,0 +1,69 @@
+import jax
+import jax.numpy as jnp
+
+def sigmoid(x):
+    return 1.0 / (1.0 + jnp.exp(-x))
+
+def elo_loss(ratings, outcome):
+    logit = (ratings * jnp.array([1.0, -1.0])).sum()
+    prob = sigmoid(logit)
+    loss = outcome * jnp.log(prob) + (1.0 - outcome) * jnp.log(1.0 - prob)
+    return loss
+
+elo_grad = jax.grad(elo_loss, argnums=0)
+
+def elo_update(idx, prev_val):
+    ratings = prev_val['ratings']
+    running_grads = prev_val['running_grads']
+    comp_idxs = prev_val['schedule'][idx,1:]
+    comp_ratings = ratings[comp_idxs]
+    outcome = prev_val['outcomes'][idx]
+    grad = elo_grad(comp_ratings, outcome)
+    new_ratings = ratings.at[comp_idxs].add(grad)
+    new_val = {
+        'ratings': new_ratings,
+        'running_grads': running_grads,
+        'schedule': schedule,
+        'outcomes': outcomes
+    }
+    return new_val
+
+if __name__ == '__main__':
+    C = 4
+    N = 10
+
+    ratings = jnp.zeros(C, dtype=jnp.float32)
+    schedule = jnp.array(
+        [[0, 0, 1],
+         [0, 1, 2],
+         [1, 1, 2],
+         [2, 0, 3],
+         [2, 3, 1],
+         [2, 3, 0],
+         [3, 2, 0],
+         [5, 2, 3],
+         [5, 3, 1],
+         [5, 1, 0]]
+    )
+    outcomes = jnp.array([1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0])
+
+    running_grads = jnp.zeros(C)
+        
+
+    init_val = {
+        'schedule': schedule,
+        'outcomes': outcomes,
+        'ratings': ratings,
+        'running_grads' : running_grads,
+    }
+    lower = 0
+    upper = schedule.shape[0]
+    new_ratings = jax.lax.fori_loop(
+        lower=lower,
+        upper=upper,
+        body_fun=elo_update,
+        init_val=init_val,
+    )
+    print(new_ratings)
+
+    # elo_update(0, init_val)
