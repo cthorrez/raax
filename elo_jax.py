f3349c8c57b7c47853b38438e1c27ecba6b75b4e
cthorrez <claytonthorrez@gmail.com>
2024-08-24 17:15:35 -0400

    perf test riix vs raax

--- elo_jax.py
+++ elo_jax.py
@@ -1,7 +1,10 @@
+import time
+import numpy as np
 import jax
 import jax.numpy as jnp
 from datasets import load_dataset
 from riix.utils.data_utils import MatchupDataset
+from riix.models.elo import Elo
 
 def sigmoid(x):
     return 1.0 / (1.0 + jnp.exp(-x))
@@ -25,10 +28,43 @@ def elo_update(idx, prev_val):
     new_val = {
         'ratings': new_ratings,
         'running_grads': running_grads,
-        'schedule': schedule,
-        'outcomes': outcomes
+        'schedule': prev_val['schedule'],
+        'outcomes': prev_val['outcomes']
     }
     return new_val
+
+
+def run_riix_elo(dataset):
+    elo = Elo(competitors=dataset.competitors)
+    elo.fit_dataset(dataset)
+    return elo.ratings
+
+def run_raax_elo(dataset):
+    C = dataset.num_competitors
+    N = len(dataset)
+    time_steps = jnp.array(dataset.time_steps)
+    matchups = jnp.array(dataset.matchups)
+    schedule = jnp.column_stack((time_steps, matchups))
+    outcomes = jnp.array(dataset.outcomes)
+    ratings = jnp.zeros(C, dtype=jnp.float32)
+    running_grads = jnp.zeros(C)
+    init_val = {
+        'schedule': schedule,
+        'outcomes': outcomes,
+        'ratings': ratings,
+        'running_grads' : running_grads,
+    }
+    lower = 0
+    upper = schedule.shape[0]
+    final_val = jax.lax.fori_loop(
+        lower=lower,
+        upper=upper,
+        body_fun=elo_update,
+        init_val=init_val,
+    )
+    new_ratings = np.asarray(final_val['ratings'])
+    return new_ratings
+
 
 if __name__ == '__main__':
     # C = 4
@@ -48,8 +84,10 @@ if __name__ == '__main__':
     # )
     # outcomes = jnp.array([1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0])
 
-
-    lol = load_dataset("EsportsBench/EsportsBench", split="league_of_legends").to_pandas()
+    start_time = time.time()
+    # split = 'league_of_legends'
+    split = 'starcraft2'
+    lol = load_dataset("EsportsBench/EsportsBench", split=split).to_pandas()
     dataset = MatchupDataset(
         df=lol,
         competitor_cols=["competitor_1", "competitor_2"],
@@ -57,31 +95,15 @@ if __name__ == '__main__':
         datetime_col="date",
         rating_period="7D"
     )
+    print(f'data load duration: {time.time() - start_time}')
 
-    C = dataset.num_competitors
-    N = len(dataset)
+    start_time = time.time()
+    riix_ratings = run_riix_elo(dataset)
+    print(f'riix fit time: {time.time() - start_time}')
 
-    time_steps = jnp.array(dataset.time_steps)
-    matchups = jnp.array(dataset.matchups)
-    schedule = jnp.column_stack((time_steps, matchups))
-    outcomes = jnp.array(dataset.outcomes)
+    start_time = time.time()
+    raxx_ratings = run_raax_elo(dataset)
+    print(f'raax fit time: {time.time() - start_time}')
 
-    ratings = jnp.zeros(C, dtype=jnp.float32)
-    running_grads = jnp.zeros(C)
-        
-
-    init_val = {
-        'schedule': schedule,
-        'outcomes': outcomes,
-        'ratings': ratings,
-        'running_grads' : running_grads,
-    }
-    lower = 0
-    upper = schedule.shape[0]
-    new_ratings = jax.lax.fori_loop(
-        lower=lower,
-        upper=upper,
-        body_fun=elo_update,
-        init_val=init_val,
-    )
-    print(new_ratings)
+    print(riix_ratings)
+    print(raxx_ratings)
