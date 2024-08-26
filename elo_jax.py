634aec46bf8e1fa91afa9b18725312b659384554
cthorrez <claytonthorrez@gmail.com>
2024-08-25 22:44:16 -0400

    ok it runs but there is an off by one in the update :/

--- elo_jax.py
+++ elo_jax.py
@@ -17,9 +17,8 @@ def elo_loss(ratings, outcome):
 
 elo_grad = jax.grad(elo_loss, argnums=0)
 
-def elo_update(idx, prev_val):
+def online_elo_update(idx, prev_val):
     ratings = prev_val['ratings']
-    running_grads = prev_val['running_grads']
     comp_idxs = prev_val['schedule'][idx,1:]
     comp_ratings = ratings[comp_idxs]
     outcome = prev_val['outcomes'][idx]
@@ -27,19 +26,56 @@ def elo_update(idx, prev_val):
     new_ratings = ratings.at[comp_idxs].add(grad)
     new_val = {
         'ratings': new_ratings,
-        'running_grads': running_grads,
         'schedule': prev_val['schedule'],
         'outcomes': prev_val['outcomes']
     }
     return new_val
 
+def online_elo_update(idx, prev_val):
+    ratings = prev_val['ratings']
+    comp_idxs = prev_val['schedule'][idx,1:]
+    comp_ratings = ratings[comp_idxs]
+    outcome = prev_val['outcomes'][idx]
+    grad = elo_grad(comp_ratings, outcome)
+    new_ratings = ratings.at[comp_idxs].add(grad)
+    new_val = {
+        'ratings': new_ratings,
+        'schedule': prev_val['schedule'],
+        'outcomes': prev_val['outcomes']
+    }
+    return new_val
 
-def run_riix_elo(dataset):
-    elo = Elo(competitors=dataset.competitors)
+def batched_elo_update(carry, x):
+    ratings = carry['ratings']
+    running_grads = carry['running_grads']
+    comp_idxs = x['schedule'][1:]
+    comp_ratings = ratings[comp_idxs]
+    outcome = x['outcomes']
+    update_flag = x['update_mask']
+    grad = elo_grad(comp_ratings, outcome)
+    new_running_grads = running_grads.at[comp_idxs].add(grad)
+
+    new_ratings = ratings + (update_flag * running_grads)
+    new_running_grads = running_grads * (1 - update_flag)
+    new_carry = {
+        'ratings': new_ratings,
+        'running_grads': new_running_grads,
+    }
+    return new_carry, None
+
+
+def run_riix_elo(dataset, mode):
+    elo = Elo(
+        competitors=dataset.competitors,
+        initial_rating=0.0,
+        alpha=1.0,
+        k=1.0,
+        update_method=mode,
+    )
     elo.fit_dataset(dataset)
     return elo.ratings
 
-def run_raax_elo(dataset):
+def run_online_raax_elo(dataset):
     C = dataset.num_competitors
     N = len(dataset)
     time_steps = jnp.array(dataset.time_steps)
@@ -47,46 +83,53 @@ def run_raax_elo(dataset):
     schedule = jnp.column_stack((time_steps, matchups))
     outcomes = jnp.array(dataset.outcomes)
     ratings = jnp.zeros(C, dtype=jnp.float32)
-    running_grads = jnp.zeros(C)
     init_val = {
         'schedule': schedule,
         'outcomes': outcomes,
         'ratings': ratings,
-        'running_grads' : running_grads,
     }
     lower = 0
     upper = schedule.shape[0]
     final_val = jax.lax.fori_loop(
         lower=lower,
         upper=upper,
-        body_fun=elo_update,
+        body_fun=online_elo_update,
         init_val=init_val,
+    )
+    new_ratings = np.asarray(final_val['ratings'])
+    return new_ratings
+
+def run_batched_raax_elo(dataset):
+    C = dataset.num_competitors
+    time_steps = jnp.array(dataset.time_steps)
+    update_mask = jnp.insert(jnp.diff(time_steps) != 0, 0, False)
+    matchups = jnp.array(dataset.matchups)
+    schedule = jnp.column_stack((time_steps, matchups))
+    outcomes = jnp.array(dataset.outcomes)
+    ratings = jnp.zeros(C, dtype=jnp.float32)
+    running_grads = jnp.zeros(C)
+    xs = {
+        'schedule': schedule,
+        'outcomes': outcomes,
+        'update_mask': update_mask,
+    }
+    init_val = {
+        'ratings': ratings,
+        'running_grads': running_grads,
+    }
+    final_val, _ = jax.lax.scan(
+        f=batched_elo_update,
+        init=init_val,
+        xs=xs,
     )
     new_ratings = np.asarray(final_val['ratings'])
     return new_ratings
 
 
 if __name__ == '__main__':
-    # C = 4
-    # N = 10
-
-    # schedule = jnp.array(
-    #     [[0, 0, 1],
-    #      [0, 1, 2],
-    #      [1, 1, 2],
-    #      [2, 0, 3],
-    #      [2, 3, 1],
-    #      [2, 3, 0],
-    #      [3, 2, 0],
-    #      [5, 2, 3],
-    #      [5, 3, 1],
-    #      [5, 1, 0]]
-    # )
-    # outcomes = jnp.array([1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0])
-
     start_time = time.time()
-    # split = 'league_of_legends'
-    split = 'starcraft2'
+    split = 'league_of_legends'
+    # split = 'starcraft2'
     lol = load_dataset("EsportsBench/EsportsBench", split=split).to_pandas()
     dataset = MatchupDataset(
         df=lol,
@@ -98,12 +141,28 @@ if __name__ == '__main__':
     print(f'data load duration: {time.time() - start_time}')
 
     start_time = time.time()
-    riix_ratings = run_riix_elo(dataset)
-    print(f'riix fit time: {time.time() - start_time}')
+    online_riix_ratings = run_riix_elo(dataset, 'iterative')
+    print(f'online riix fit time: {time.time() - start_time}')
 
     start_time = time.time()
-    raxx_ratings = run_raax_elo(dataset)
-    print(f'raax fit time: {time.time() - start_time}')
+    online_raxx_ratings = run_online_raax_elo(dataset)
+    print(f'online raax fit time: {time.time() - start_time}')
 
-    print(riix_ratings)
-    print(raxx_ratings)
+    start_time = time.time()
+    batched_riix_ratings = run_riix_elo(dataset, 'batched')
+    print(f'online batched fit time: {time.time() - start_time}')
+
+    start_time = time.time()
+    batched_raxx_ratings = run_batched_raax_elo(dataset)
+    print(f'batched raax fit time: {time.time() - start_time}')
+
+    print('online diffs:')
+    print(np.min(np.abs(online_riix_ratings - online_raxx_ratings)))
+    print(np.max(np.abs(online_riix_ratings - online_raxx_ratings)))
+    print(np.mean(np.abs(online_riix_ratings - online_raxx_ratings)))
+
+    print('batched diffs:')
+    print(np.min(np.abs(batched_riix_ratings - batched_raxx_ratings)))
+    print(np.max(np.abs(batched_riix_ratings - batched_raxx_ratings)))
+    print(np.mean(np.abs(batched_riix_ratings - batched_raxx_ratings)))
+
