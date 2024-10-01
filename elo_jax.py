dd797e234d0fa9beea2200277197fda79f1b19b2
cthorrez <claytonthorrez@gmail.com>
2024-10-01 01:57:01 -0700

    ok flaot64 is actually necessary for Elo thinking emoji

--- elo_jax.py
+++ elo_jax.py
@@ -1,8 +1,10 @@
+import math
 import time
 from functools import partial
 import numpy as np
 from contextlib import contextmanager
 import jax
+jax.config.update("jax_enable_x64", True)
 import jax.numpy as jnp
 from data_utils import load_dataset
 from riix.utils.data_utils import MatchupDataset
@@ -28,24 +30,25 @@ def elo_loss(ratings, outcome):
 
 elo_grad = jax.grad(elo_loss, argnums=0)
 
-@jax.jit
-def elo_grad_hard(ratings, outcome):
-    prob = jax.nn.sigmoid(ratings[0] - ratings[1])
+@partial(jax.jit, static_argnums=(2,))
+def elo_grad_hard(ratings, outcome, alpha):
+    prob = jax.nn.sigmoid(alpha*(ratings[0] - ratings[1]))
     grad = outcome - prob
     return grad
 
 vec_elo_grad_hard = jax.jit(jax.vmap(elo_grad_hard))
 
 
-def online_elo_update(idx, prev_val):
+def online_elo_update(idx, prev_val, alpha, k):
     ratings = prev_val['ratings']
     comp_idxs = prev_val['matchups'][idx]
     comp_ratings = ratings[comp_idxs]
     outcome = prev_val['outcomes'][idx]
-    grad = elo_grad_hard(comp_ratings, outcome)
+    grad = elo_grad_hard(comp_ratings, outcome, alpha)
     # grad = elo_grad(comp_ratings, outcome)
-    new_ratings = ratings.at[comp_idxs[0]].add(grad)
-    new_ratings = new_ratings.at[comp_idxs[1]].add(-grad)
+    update = k * grad
+    new_ratings = ratings.at[comp_idxs[0]].add(update)
+    new_ratings = new_ratings.at[comp_idxs[1]].add(-update)
     new_val = {
         'ratings': new_ratings,
         'matchups': prev_val['matchups'],
@@ -57,58 +60,52 @@ def run_riix_elo(dataset, mode):
     elo = Elo(
         competitors=dataset.competitors,
         initial_rating=0.0,
-        alpha=1.0,
-        k=1.0,
+        alpha=math.log(10.0) / 400.0,
+        k=32.0,
         update_method=mode,
     )
     elo.fit_dataset(dataset)
     return elo.ratings
 
-# Next ideas:
-# 1. try to find max competitors active in a period and use a vec of indices of that size
-# 2. do masking on batches of max size in time dimension and use standard vmap
-# 3. try GPU
 @jax.jit
-def do_update(ratings, running_grads):
-    return running_grads, running_grads
+def do_update(stale_ratings, fresh_ratings):
+    return jnp.copy(fresh_ratings), jnp.copy(fresh_ratings)
 
 @jax.jit
-def do_nothing(ratings, running_grads):
-    return ratings, running_grads
+def do_nothing(stale_ratings, fresh_ratings):
+    return stale_ratings, fresh_ratings
 
-def batched_elo_update(carry, x):
-    ratings = carry['ratings']
-    running_grads = carry['running_grads']
+@partial(jax.jit, static_argnums=(2,))
+def batched_elo_update(carry, x, alpha, k):
+    stale_ratings = carry['stale_ratings']
+    fresh_ratings = carry['fresh_ratings']
     comp_idxs = x['matchups']
     update_flag = x['update_mask']
     outcome = x['outcomes']
 
-    ratings, running_grads = jax.lax.cond(
+    stale_ratings, fresh_ratings = jax.lax.cond(
         update_flag,
         do_update,
         do_nothing,
-        ratings,
-        running_grads,
+        stale_ratings,
+        fresh_ratings,
     )
-
-    # ratings = ratings + (update_flag * running_grads)
-    # running_grads = running_grads * (1 - update_flag)
-
-    comp_ratings = ratings[comp_idxs]
+    comp_ratings = stale_ratings[comp_idxs]
 
     # grad = elo_grad(comp_ratings, outcome)
-    grad = elo_grad_hard(comp_ratings, outcome)
-    new_running_grads = running_grads.at[comp_idxs[0]].add(grad)
-    new_running_grads = new_running_grads.at[comp_idxs[1]].add(-grad)
+    grad = elo_grad_hard(comp_ratings, outcome, alpha)
+    update = k * grad
+    fresh_ratings = fresh_ratings.at[comp_idxs[0]].add(update)
+    fresh_ratings = fresh_ratings.at[comp_idxs[1]].add(-update)
 
     new_carry = {
-        'ratings': ratings,
-        'running_grads': new_running_grads,
+        'stale_ratings': stale_ratings,
+        'fresh_ratings': fresh_ratings,
     }
     return new_carry, None
 
-def run_online_raax_elo(matchups, outcomes, num_competitors):
-    ratings = jnp.zeros(num_competitors, dtype=jnp.float32)
+def run_online_raax_elo(matchups, outcomes, num_competitors, alpha=math.log(10.0)/400.0, k=32.0):
+    ratings = jnp.zeros(num_competitors, dtype=jnp.float64)
     init_val = {
         'matchups': matchups,
         'outcomes': outcomes,
@@ -119,33 +116,31 @@ def run_online_raax_elo(matchups, outcomes, num_competitors):
     final_val = jax.lax.fori_loop(
         lower=lower,
         upper=upper,
-        body_fun=online_elo_update,
+        body_fun=partial(online_elo_update, alpha=alpha, k=k),
         init_val=init_val,
     )
     new_ratings = final_val['ratings']
     return new_ratings
 
-def run_batched_raax_elo(matchups, outcomes, update_mask, num_competitors):
-    ratings = jnp.zeros(num_competitors, dtype=jnp.float32)
-    running_grads = jnp.zeros(num_competitors, dtype=jnp.float32)
+def run_batched_raax_elo(matchups, outcomes, update_mask, num_competitors, alpha=math.log(10.0) / 400.0, k=32.0):
+    stale_ratings = jnp.zeros(num_competitors, dtype=jnp.float64)
+    fresh_ratings = jnp.zeros(num_competitors, dtype=jnp.float64)
     xs = {
         'matchups': matchups,
         'outcomes': outcomes,
         'update_mask': update_mask,
     }
     init_val = {
-        'ratings': ratings,
-        'running_grads': running_grads,
+        'stale_ratings': stale_ratings,
+        'fresh_ratings': fresh_ratings
     }
     final_val, _ = jax.lax.scan(
-        f=batched_elo_update,
+        f=partial(batched_elo_update, alpha=alpha, k=k),
         init=init_val,
         xs=xs,
         length=matchups.shape[0],
-        unroll=20,
     )
-    new_ratings = final_val['ratings'] + final_val['running_grads']
-    return new_ratings
+    return final_val['fresh_ratings']
 
 
 def jax_preprocess(dataset):
@@ -153,7 +148,6 @@ def jax_preprocess(dataset):
     _, counts = jnp.unique_counts(time_steps)
     end_idxs = jnp.cumsum(counts)
     start_idxs = jnp.concatenate([jnp.array([0]), end_idxs[:-1]])
-
     update_mask = jnp.insert(jnp.diff(time_steps) != 0, 0, False)
     matchups = jnp.array(dataset.matchups)
     outcomes = jnp.array(dataset.outcomes)
@@ -161,8 +155,10 @@ def jax_preprocess(dataset):
 
 
 if __name__ == '__main__':
-    # dataset = load_dataset("league_of_legends", '28D')
-    dataset = load_dataset("smash_melee", '1D')
+    dataset = load_dataset("league_of_legends", '7D')
+    # dataset = load_dataset("starcraft2", '1D')
+    # dataset = load_dataset("smash_melee", '1D')
+
 
     matchups, outcomes, update_mask, start_idxs, end_idxs = jax_preprocess(dataset)
 
@@ -187,7 +183,6 @@ if __name__ == '__main__':
     print(np.min(np.abs(batched_riix_ratings - batched_raax_ratings)))
     print(np.max(np.abs(batched_riix_ratings - batched_raax_ratings)))
     print(np.mean(np.abs(batched_riix_ratings - batched_raax_ratings)))
-    print('vmap diffs:')
-    # print(np.min(np.abs(batched_riix_ratings - vmap_raax_ratings)))
-    # print(np.max(np.abs(batched_riix_ratings - vmap_raax_ratings)))
-    # print(np.mean(np.abs(batched_riix_ratings - vmap_raax_ratings)))
+
+    print(online_riix_ratings)
+    print(online_raax_ratings)
